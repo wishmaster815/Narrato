e80349e89b76394418b89e9df51cf70e4cb0e5cc
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-08-18 23:57:16 +0530

    add nodes and builder

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -1,6 +1,7 @@
 from langgraph.graph import START, END, StateGraph
 from src.llms.groqllm import GroqLLM
 from src.states.blog_state import BlogState
+from src.nodes.blog_node import BlogNode
 class GraphBuilder:
     def __init__(self, llm):
         self.llm = llm
@@ -10,9 +11,11 @@ class GraphBuilder:
         """
         create a graph to generate content
         """
+
+        blog_node_obj = BlogNode(llm = self.llm)
         # nodes
-        self.builder.add_node("title_creation","")
-        self.builder.add_node("content_generator","")
+        self.builder.add_node("title_creation",blog_node_obj.title_creation)
+        self.builder.add_node("content_generator",blog_node_obj.content_generation)
 
         # edges
         self.builder.add_edge(START, "title_creation")
---
+++ src/nodes/blog_node.py
@@ -0,0 +1,33 @@
+from src.states.blog_state import BlogState
+class BlogNode:
+    def __init__(self,llm):
+        self.llm = llm
+    
+    def title_creation(self, state:BlogState):
+        """
+        create a title for the blog
+        """
+        if "topic" in state and state["topic"]:
+            prompt = """
+                        You are an exprt blog content writer. Use markdown formatting. Generate a blog title for the topic {topic}. This title should be creatuve and SEo friendly.
+                    """
+            system_message = prompt.format(topic = state["topic"])
+            response = self.llm.invoke(system_message)
+            return {
+                "blog":{"title":response.content}
+            }
+
+    def content_generation(self, state:BlogState):
+        """
+        create a detailed blog on the given topic
+        """
+        if "topic" in state and state["topic"]:
+            prompt = """You are an exprt blog content writer. Use markdown formatting.create a detailed blog content with detailed breakdown of the topic"""
+            system_message = prompt.format(topic = state["topic"])
+            response = self.llm.invoke(system_message)
+            return {
+                "blog":{
+                    "title":state["blog"]["title"],
+                    "content":response.content
+                }
+            }
